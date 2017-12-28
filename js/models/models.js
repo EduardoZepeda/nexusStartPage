4b4c6bc013b0ad2081af67fc155939e140fc0a16
Eduardo Zepeda <magallon_zepeda_carlos@hotmail.com>
2017-12-28 16:35:43 -0600

    Removes the # character from url validation

--- js/models/models.js
+++ js/models/models.js
@@ -7,15 +7,15 @@ var URL = Backbone.Model.extend({
   },
 
   validate: function(attrs) {
-    var regex = /[#$^()\[\]{}|<>]/g;
+    var regex = /[$^()\[\]{}|<>]/g;
     $('.successMsg').text(""); //Delete success msg
     if (attrs.name.length <= 1) {
       $("#ErrorName").text(" *The name must be longer than one character. ");
       return "The name must be longer than one character"
     }
     if (regex.test(attrs.name)) {
-      $("#ErrorName").text(" *No #$^()[]{}|<> is allowed");
-      return "No #$^()[]{}|<> is allowed"
+      $("#ErrorName").text(" *No $^()[]{}|<> is allowed");
+      return "No $^()[]{}|<> is allowed"
     }
     $("#ErrorName").text("");
     if (attrs.url.length <= 1) {
@@ -23,8 +23,8 @@ var URL = Backbone.Model.extend({
       return "The URL must be longer than one character"
     }
     if(regex.test(attrs.url)){
-      $("#errorUrl").text(" *No #$^()[]{}|<> is allowed");
-      return "No #$^()[]{}|<> is allowed"
+      $("#errorUrl").text(" *No $^()[]{}|<> is allowed");
+      return "No $^()[]{}|<> is allowed"
     }
     $("#errorUrl").text("");
   }
